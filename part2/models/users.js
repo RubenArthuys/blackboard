5042a27bbc9053ba5756d6a21e216ba55715287a
RubenArthuys <ruben.arthuys@gmail.com>
2022-06-28 18:15:18 +0200

    part2

---
+++ part2/models/users.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+
+var messageSchema = mongoose.Schema({
+  title: String,
+  content: String,
+  dateExp: Date,
+  read: Boolean,
+  sender: String,
+})
+
+var taskSchema = mongoose.Schema({
+  name: String,
+  category: String,
+  owner: String,
+  dateInsert: Date,
+  dateDue: Date,
+  dateCloture: Date,
+
+})
+
+var usersSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  age: Number,
+  status: String,
+  gender: String,
+  dateInsert: Date,
+  messages: [messageSchema],
+  tasks: [taskSchema],
+})
+
+
+var userModel = mongoose.model('users', usersSchema)
+
+module.exports = userModel
---
+++ part2/routes/index.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var router = express.Router();
+
+var articlesModel = require('../models/articles');
+var ordersModel = require('../models/orders');
+var userModel = require('../models/users');
+
+
+// Home page
+router.get('/', async function(req, res, next) {
+
+  var articlesZero = await articlesModel.find({ stock : 0 });
+
+  var admin = await userModel.findById('5c52e4efaa4beef85aad5e52')
+  
+  var messages = admin.messages
+  let unreadMessages = 0
+  for(var i=0; i<messages.length; i++) {
+    if(messages[i].read == false) {
+      unreadMessages ++
+    }
+  }
+  // console.log(unreadMessages);
+
+  var tasks = admin.tasks
+  let tasksUndone = 0
+  for(var i=0; i<tasks.length; i++) {
+    if(tasks[i].dateCloture == null) {
+      tasksUndone ++
+    }
+  }
+
+  res.render('index', { articlesZero : articlesZero.length, unreadMessages, tasksUndone });
+});
+
+// Tasks page
+router.get('/tasks-page', async function(req, res, next) {
+  var admin = await userModel.findById("5c52e4efaa4beef85aad5e52")
+  //On va piocher juste les tâches de admin
+  var adminTasks = admin.tasks
+  res.render('tasks', { adminTasks });
+});
+
+//Messages page
+router.get('/messages-page', async function(req, res, next) {
+  var admin = await userModel.findById("5c52e4efaa4beef85aad5e52")
+  // console.log(admin.messages)
+  var adminMessages = admin.messages
+
+  res.render('messages', { adminMessages });
+});
+
+//Users page
+router.get('/users-page', async function(req, res, next) {
+  var users = await userModel.find({ status : "customer" });
+  res.render('users', { users });
+});
+
+// Catalog page
+router.get('/catalog-page', async function(req, res, next) {
+  var articlesFromDB = await articlesModel.find();
+  res.render('catalog', { articlesEJS : articlesFromDB });
+});
+
+// Orders LIST page
+router.get('/orders-list-page', async function(req, res, next) {
+  var orders = await ordersModel.find();
+  res.render('orders-list', { ordersEJS : orders });
+});
+
+// Order DETAILS page
+router.get('/order-page', async function(req, res, next) {
+  var orderDetails = await ordersModel.findById(req.query.id)
+                                      .populate('articles')
+                                      .exec();
+  //populate() va récupérer les articles associé à une order, cela rempli l'information dans la réference.
+  //Def : Remplace le chemin dans le document d'une collection avec le document d'une autre collection.
+  res.render('order', { orderDetails });
+});
+
+// Charts page
+router.get('/charts', function(req, res, next) {
+  res.render('charts');
+});
+
+
+module.exports = router;
